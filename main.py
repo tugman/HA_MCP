7175d1580ae08f96bdbdf4434dcf78037c8dc76f
Francois <frabernier@gmail.com>
2025-05-15 15:40:14 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+# Import
+from mcp.server.fastmcp import FastMCP
+
+
+
+# Instantiate the MCP server client
+mcp = FastMCP("Hello, and welcome to HA MCP :-)")
+
+
+
+
+# execute and return the sse output
+if __name__ == "__main__":
+    mcp.run(transport="sse")
+
+
+# To Do
+
