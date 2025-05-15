821d4bc9e01ff142ffb522f1f64d516786819144
Francois BERNIER <frabernier@gmail.com>
2025-05-15 16:05:54 +0200

    Update

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-# Import
+# Imports
 from mcp.server.fastmcp import FastMCP
 
+
+
+
 # Instantiate the MCP server client
-mcp = FastMCP("Hello, and welcome to HA MCP :-)")
+mcp = FastMCP("Hello, and welcome to HA MCP:-)")
 
 
-# execute and return the sse output
+
+
+# To Do define MCP tools like :
+#@mcp.tool()
+#def cos(a: int) -> float:
+#    """cos of a number"""
+#    return float(math.cos(a))
+
+
+
+
+
+
+
+
+
+# Execute and return the sse output
 if __name__ == "__main__":
     mcp.run(transport="sse")
-
-
-# To Do
-
-
